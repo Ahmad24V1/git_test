98ec22a3f78d6d6dcd82f76b0b6c6cf9761ba0c5
Ahmad Al Kaissi <160283058+Ahmad24V1@users.noreply.github.com>
2024-11-06 00:32:34 +0200

    Update

---
+++ ahmad.php
@@ -0,0 +1,110 @@
+<table cellpadding="1" cellspacing="1" id="heroes" class="row_table_data">
+	<thead>
+		
+		<tr>
+			<th colspan='2' style='width:50%;'>لاعبون</th><th  style='padding: 2px 2px;width:1%;'>مستوى</th><th  style='padding: 2px 2px;width:5%;'>XP</th><th  style='padding: 2px 2px;width:1%;'>%هجوم</th><th  style='padding: 2px 2px;width:1%;'>%دفاع</th><th  style='padding: 2px 2px;width:1%;'>السرعه</th><th  style='padding: 2px 2px;width:1%;'>حياة</th>
+		</tr>
+	</thead>
+	<tbody>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">1</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u21" alt="كتيبة" title="كتيبة" src="assets/x.gif"> &nbsp;<a   href="spieler?uid=639">Carla</a></td>
+		   <td style='padding: 2px 2px;' class="xp">424</td><td style='padding: 2px 2px;' class="xp">14,569,281,805</td><td style='padding: 2px 2px;' class="xp">100%</td><td style='padding: 2px 2px;' class="xp">100%</td><td style='padding: 2px 2px;' class="xp">250%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">2</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u104" alt="قنّاص" title="قنّاص" src="assets/x.gif"> &nbsp;<a style='color: green !important;'  href="spieler?uid=331">See you</a></td>
+		   <td style='padding: 2px 2px;' class="xp">341</td><td style='padding: 2px 2px;' class="xp">8,167,725,741</td><td style='padding: 2px 2px;' class="xp">80%</td><td style='padding: 2px 2px;' class="xp">80%</td><td style='padding: 2px 2px;' class="xp">200%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">3</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u56" alt="عربة رشب" title="عربة رشب" src="assets/x.gif"> &nbsp;<a style='color: green !important;'  href="spieler?uid=261">912</a></td>
+		   <td style='padding: 2px 2px;' class="xp">297</td><td style='padding: 2px 2px;' class="xp">5,683,767,189</td><td style='padding: 2px 2px;' class="xp">60%</td><td style='padding: 2px 2px;' class="xp">60%</td><td style='padding: 2px 2px;' class="xp">144%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">4</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u104" alt="قنّاص" title="قنّاص" src="assets/x.gif"> &nbsp;<a   href="spieler?uid=449">👤</a></td>
+		   <td style='padding: 2px 2px;' class="xp">283</td><td style='padding: 2px 2px;' class="xp">4,995,216,922</td><td style='padding: 2px 2px;' class="xp">60%</td><td style='padding: 2px 2px;' class="xp">46.4%</td><td style='padding: 2px 2px;' class="xp">150%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">5</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u16" alt="فارس الجرمان" title="فارس الجرمان" src="assets/x.gif"> &nbsp;<a style='color: green !important;'  href="spieler?uid=555">قلم</a></td>
+		   <td style='padding: 2px 2px;' class="xp">276</td><td style='padding: 2px 2px;' class="xp">4,706,312,350</td><td style='padding: 2px 2px;' class="xp">60%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">150%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">6</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u104" alt="قنّاص" title="قنّاص" src="assets/x.gif"> &nbsp;<a   href="spieler?uid=89">🦂</a></td>
+		   <td style='padding: 2px 2px;' class="xp">233</td><td style='padding: 2px 2px;' class="xp">2,992,880,711</td><td style='padding: 2px 2px;' class="xp">60%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">150%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">7</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u65" alt="فارس إلبيدا" title="فارس إلبيدا" src="assets/x.gif"> &nbsp;<a style='color: green !important;'  href="spieler?uid=366">❤️</a></td>
+		   <td style='padding: 2px 2px;' class="xp">210</td><td style='padding: 2px 2px;' class="xp">2,271,582,645</td><td style='padding: 2px 2px;' class="xp">60%</td><td style='padding: 2px 2px;' class="xp">23.6%</td><td style='padding: 2px 2px;' class="xp">61%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">8</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u61" alt="هوبليت" title="هوبليت" src="assets/x.gif"> &nbsp;<a style='color: green !important;'  href="spieler?uid=3">برعي</a></td>
+		   <td style='padding: 2px 2px;' class="xp">191</td><td style='padding: 2px 2px;' class="xp">1,770,308,663</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">40%</td><td style='padding: 2px 2px;' class="xp">82%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">9</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u104" alt="قنّاص" title="قنّاص" src="assets/x.gif"> &nbsp;<a   href="spieler?uid=114">ليش</a></td>
+		   <td style='padding: 2px 2px;' class="xp">170</td><td style='padding: 2px 2px;' class="xp">1,315,002,316</td><td style='padding: 2px 2px;' class="xp">40%</td><td style='padding: 2px 2px;' class="xp">29.6%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">10</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u105" alt="صعلوك" title="صعلوك" src="assets/x.gif"> &nbsp;<a   href="spieler?uid=171">🧌</a></td>
+		   <td style='padding: 2px 2px;' class="xp">146</td><td style='padding: 2px 2px;' class="xp">873,016,139</td><td style='padding: 2px 2px;' class="xp">40%</td><td style='padding: 2px 2px;' class="xp">6.4%</td><td style='padding: 2px 2px;' class="xp">100%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">11</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u56" alt="عربة رشب" title="عربة رشب" src="assets/x.gif"> &nbsp;<a style='color: green !important;'  href="spieler?uid=179">✈</a></td>
+		   <td style='padding: 2px 2px;' class="xp">139</td><td style='padding: 2px 2px;' class="xp">776,371,239</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">40%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">12</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u16" alt="فارس الجرمان" title="فارس الجرمان" src="assets/x.gif"> &nbsp;<a   href="spieler?uid=125">ندّم</a></td>
+		   <td style='padding: 2px 2px;' class="xp">130</td><td style='padding: 2px 2px;' class="xp">640,722,917</td><td style='padding: 2px 2px;' class="xp">40%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">100%</td><td style='padding: 2px 2px;' class="xp"><b style="color: red !important;">ميت</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">13</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u55" alt="حارس أنحور" title="حارس أنحور" src="assets/x.gif"> &nbsp;<a style='color: green !important;'  href="spieler?uid=215">5</a></td>
+		   <td style='padding: 2px 2px;' class="xp">119</td><td style='padding: 2px 2px;' class="xp">510,048,805</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">100%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">14</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u76" alt="الڤالكيري" title="الڤالكيري" src="assets/x.gif"> &nbsp;<a style='color: green !important;'  href="spieler?uid=22">سلمان</a></td>
+		   <td style='padding: 2px 2px;' class="xp">118</td><td style='padding: 2px 2px;' class="xp">501,743,410</td><td style='padding: 2px 2px;' class="xp">14.4%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">100%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">15</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u16" alt="فارس الجرمان" title="فارس الجرمان" src="assets/x.gif"> &nbsp;<a   href="spieler?uid=1056">🧊</a></td>
+		   <td style='padding: 2px 2px;' class="xp">117</td><td style='padding: 2px 2px;' class="xp">491,796,620</td><td style='padding: 2px 2px;' class="xp">40%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">100%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">16</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u56" alt="عربة رشب" title="عربة رشب" src="assets/x.gif"> &nbsp;<a   href="spieler?uid=137">مرزبا</a></td>
+		   <td style='padding: 2px 2px;' class="xp">115</td><td style='padding: 2px 2px;' class="xp">464,033,204</td><td style='padding: 2px 2px;' class="xp">0.8%</td><td style='padding: 2px 2px;' class="xp">40%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp"><b style="color: red !important;">ميت</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">17</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u26" alt="فارس الهيدوانر" title="فارس الهيدوانر" src="assets/x.gif"> &nbsp;<a style='color: green !important;'  href="spieler?uid=19">⭕</a></td>
+		   <td style='padding: 2px 2px;' class="xp">110</td><td style='padding: 2px 2px;' class="xp">419,565,601</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">40%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">18</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u16" alt="فارس الجرمان" title="فارس الجرمان" src="assets/x.gif"> &nbsp;<a style='color: green !important;'  href="spieler?uid=86">48</a></td>
+		   <td style='padding: 2px 2px;' class="xp">101</td><td style='padding: 2px 2px;' class="xp">332,546,149</td><td style='padding: 2px 2px;' class="xp">0.8%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">100%</td><td style='padding: 2px 2px;' class="xp"><b style="color: green !important;">حي</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">19</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u5" alt="سلاح الفرسان" title="سلاح الفرسان" src="assets/x.gif"> &nbsp;<a   href="spieler?uid=12">X</a></td>
+		   <td style='padding: 2px 2px;' class="xp">96</td><td style='padding: 2px 2px;' class="xp">292,694,861</td><td style='padding: 2px 2px;' class="xp">16.6%</td><td style='padding: 2px 2px;' class="xp">16.8%</td><td style='padding: 2px 2px;' class="xp">38%</td><td style='padding: 2px 2px;' class="xp"><b style="color: red !important;">ميت</b></td>
+		</tr>
+				<tr>
+		   <td style='padding: 2px 2px;' class="ra">20</td>
+		   <td style='padding: 2px 2px;' class="pla"><img class="unit u11" alt="مهاجم بهراوة" title="مهاجم بهراوة" src="assets/x.gif"> &nbsp;<a   href="spieler?uid=59">P</a></td>
+		   <td style='padding: 2px 2px;' class="xp">94</td><td style='padding: 2px 2px;' class="xp">275,947,173</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp">0%</td><td style='padding: 2px 2px;' class="xp"><b style="color: red !important;">ميت</b></td>
+		</tr>
+		 </tbody>
+</table>
